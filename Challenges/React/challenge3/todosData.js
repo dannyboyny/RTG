5fb0204279a97c835d697d176eb3cae11b8cda5d
Daniel Hernandez <4128621+dannyboyny@users.noreply.github.com>
2019-12-11 23:44:34 -0500

    React Challenge 3
    React Challenge 3

---
+++ Challenges/React/challenge3/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Author items",
+    completed: true
+  },
+  {
+    id: 2,
+    text: "Create Human Scoring Batches",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Run Automated Scoring",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "Get Data Extraction",
+    completed: true
+  },
+  {
+    id: 5,
+    text: "Do React Homework",
+    completed: false
+  }
+]
+
+export default todosData
