cb950bf3ccd866d538aad469925f1aa03d0fdee0
Daniel Hernandez <4128621+dannyboyny@users.noreply.github.com>
2020-01-01 22:47:45 -0500

    Challenge 5
    Challenge 5: React Todo App: Phase 6

---
+++ Challenges/React/challenge5/README.md
@@ -0,0 +1,6 @@
+# Challenge 5
+
+type "npm install" to download node modules
+type "npm start" to start app
+
+React Todo App: Phase 6
---
+++ Challenges/React/challenge5/src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+  {
+      id: 1,
+      text: "Ensure accuracy of content & functionality.",
+      completed: true
+  },
+  {
+      id: 2,
+      text: "Validate data is captured properly.",
+      completed: false
+  },
+  {
+      id: 3,
+      text: "Request PI/project team do the above, by email, copying manager leading project.",
+      completed: false
+  },
+  {
+      id: 4,
+      text: "Tale the test using exact steps a test taker would go through.",
+      completed: true
+  },
+  {
+      id: 5,
+      text: "Validate score data is captured properly.",
+      completed: false
+  }
+]
+
+export default todosData
