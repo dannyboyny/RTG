a77608e9bd108d8c2f6474b29b1460d72ea4b94a
Daniel Hernandez <4128621+dannyboyny@users.noreply.github.com>
2020-01-09 12:18:20 -0500

    Challenge 6
    React Conditional Render

---
+++ Challenges/React/challenge6/README.md
@@ -0,0 +1,6 @@
+# Challenge 6
+
+type "npm install" to download node modules
+type "npm start" to start app
+
+React Conditional Render Practice
---
+++ Challenges/React/challenge6/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = env => {
+  return {
+    entry: [path.join(__dirname, 'src/index.js')],
+    output: {
+      filename: 'main.js',
+      path: path.join(__dirname, '/dist')
+    },
+    module: {
+      rules: [
+        {
+        test: /\.js$/,
+        // exclude: /node_modules/,
+        loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/, use:['style-loader', 'css-loader']
+        }
+      ]
+    },
+    mode: 'development',
+    plugins: [
+      new HtmlWebpackPlugin ({
+        template: 'src/index.html'
+      })
+    ]
+  };
+};
